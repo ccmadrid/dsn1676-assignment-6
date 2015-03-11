51f1138e2b5dc10baf6876499a72f55eb1c135ed
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2015-03-11 14:14:37 -0400

    tweaked animations

--- js/main.js
+++ js/main.js
@@ -6,38 +6,36 @@ var $list = $('.list');
 
 $('.btn-show-hide').on('click', function (e) {
 	e.preventDefault();
-	console.log('clicked-show-hide')
+	// console.log('clicked-show-hide')
 	$box.toggleClass('js-show-hide');
 });
 
-
 $('.btn-move').on('click', function (e) {
 	e.preventDefault();
-	console.log('clicked-move')
+	// console.log('clicked-move')
 	$diamond.toggleClass('js-move');
 });
 
 $('.btn-collapse-expand').on('click', function (e) {
 	e.preventDefault();
-	console.log('clicked-collapse-expand')
+	// console.log('clicked-collapse-expand')
 	$panel.animate({height: 'toggle'});
 });
 
 $('.btn-bounce').on('click', function (e) {
 	e.preventDefault();
-	console.log('clicked-bounce')
+	// console.log('clicked-bounce')
 	$circle.addClass('js-bounce');
 });
 
 $circle.on('webkitAnimationEnd animationend', function() {
-	console.log('animation-end');
+	// console.log('animation-end');
 	$circle.removeClass('js-bounce');
 });
 
 $('.btn-append').on('click', function (e) {
 	e.preventDefault();
-	console.log('clicked-append')
+	// console.log('clicked-append')
 	var $li = $('<li>').html('New List Item');
-
 	$list.prepend($li);
 });
