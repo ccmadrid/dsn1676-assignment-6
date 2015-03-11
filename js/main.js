41dd2b0378e8b350c039979e4913a11acbae7406
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2015-03-11 13:39:00 -0400

    added other button functions

--- js/main.js
+++ js/main.js
@@ -6,6 +6,37 @@ var $list = $('.list');
 
 $('.btn-show-hide').on('click', function (e) {
 	e.preventDefault();
-	console.log('clicked')
+	console.log('clicked-show-hide')
 	$box.toggleClass('js-show-hide');
+});
+
+
+$('.btn-move').on('click', function (e) {
+	e.preventDefault();
+	console.log('clicked-move')
+	$diamond.toggleClass('js-move');
+});
+
+$('.btn-collapse-expand').on('click', function (e) {
+	e.preventDefault();
+	console.log('clicked-collapse-expand')
+	$panel.animate({height: 'toggle'});
+});
+
+$('.btn-bounce').on('click', function (e) {
+	e.preventDefault();
+	console.log('clicked-bounce')
+	$circle.addClass('js-bounce');
+});
+
+$circle.on('animationend', function() {
+	$circle.removeClass('js-bounce');
+});
+
+$('.btn-append').on('click', function (e) {
+	e.preventDefault();
+	console.log('clicked-append')
+	var $li = $('<li>').html('New List Item');
+
+	$list.prepend($li);
 });
